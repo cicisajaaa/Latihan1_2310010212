3ec9eb7156851d3a593f9e466f359771c162d8bd
cicisajaaa <shahlaqeysha@gmail.com>
2025-06-09 23:56:52 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -2,5 +2,15 @@
 package hitung;
 
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        // Membuat objek 
+        Matematika cici = new Matematika(1, 2);
+        
+        System.out.println("Hasil Penjumlahan: " +cici.setPenjumlahan());
+        System.out.println("Hasil Pengurangan: " +cici.setPengurangan());
+        System.out.println("Hasil Perkalian: " +cici.setPerkalian());
+        System.out.println("Hasil Pembagian: " +cici.setPembagian());
+        
+        
+    }
 }
